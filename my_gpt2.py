eba86845219ab528b0129ad968cd3d4f3428f7ba
Zhao Shenyang <dev@zsy.im>
2024-01-09 20:16:40 +0800

    add kv cache

--- my_gpt2.py
+++ my_gpt2.py
@@ -23,7 +23,7 @@ def linear(x, w, b):  # [m, in], [in, out], [out] -> [m, out]
     return x @ w + b
 
 
-def gpt2(inputs, wte, wpe, blocks, ln_f, n_head): # [n_seq] -> [n_seq, n_vocab]
+def gpt2(inputs, wte, wpe, blocks, ln_f, n_head, kv_cache=None): # [n_seq] -> [n_seq, n_vocab]
     # "wpe": [n_ctx, n_embd], positional embeddings
     # "wte": [n_vocab, n_embd], lookup table for vocab embeddings
     # "ln_f": {"b": [n_embd], "g": [n_embd]},
@@ -44,23 +44,36 @@ def gpt2(inputs, wte, wpe, blocks, ln_f, n_head): # [n_seq] -> [n_seq, n_vocab]
     # token + positional embeddings
     # wte[inputs] -> [n_seq, n_embd]]
     # wpe[range(len(inputs))] -> [[n_seq, n_embd]]
-    x = wte[inputs] + wpe[range(len(inputs))]  # [n_seq] -> [n_seq, n_embd]
-    for block in blocks:
-        x = transformer_block(x, **block, n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
+
+    if kv_cache is None:
+        kv_cache = [None] * len(blocks)
+        wpe_out = wpe[range(len(inputs))]
+    else:
+        wpe_out = wpe[[len(inputs) - 1]]
+        inputs = [inputs[-1]]
+
+    x = wte[inputs] + wpe_out  # [n_seq] -> [n_seq, n_embd]
+
+    new_kv_cache = []
+    for idx, block in enumerate(blocks):
+        block_kv_cache = kv_cache[idx]
+        x, updated_cache = transformer_block(x, **block, n_head=n_head, kv_cache=block_kv_cache)  # [n_seq, n_embd] -> [n_seq, n_embd]
+        new_kv_cache.append(updated_cache)
 
     # projection to vocab
     x = layer_norm(x, **ln_f)  # [n_seq, n_embd] -> [n_seq, n_embd]
-    return x @ wte.T  # [n_seq, n_embd] -> [n_seq, n_vocab]
+    return x @ wte.T, new_kv_cache  # [n_seq, n_embd] -> [n_seq, n_vocab]
 
 
-def transformer_block(x, mlp, attn, ln_1, ln_2, n_head):  # [n_seq, n_embd] -> [n_seq, n_embd]
+def transformer_block(x, mlp, attn, ln_1, ln_2, n_head, kv_cache=None):  # [n_seq, n_embd] -> [n_seq, n_embd]
     # multi-head causal self attention
-    x = x + mha(layer_norm(x, **ln_1), **attn, n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
+    attn_out, kv_cache_updated = mha(layer_norm(x, **ln_1), **attn, n_head=n_head, kv_cache=kv_cache)
+    x = x + attn_out  # [n_seq, n_embd] -> [n_seq, n_embd]
 
     # position-wise feed forward network
     x = x + ffn(layer_norm(x, **ln_2), **mlp)  # [n_seq, n_embd] -> [n_seq, n_embd]
 
-    return x
+    return x, kv_cache_updated
 
 
 def ffn(x, c_fc, c_proj):
@@ -77,18 +90,31 @@ def attention(q, k, v, mask):  # [n_q, d_k], [n_k, d_k], [n_k, d_v], [n_q, n_k] 
     return softmax(q @ k.T / np.sqrt(q.shape[-1]) + mask) @ v
 
 
-def mha(x, c_attn, c_proj, n_head): # [n_seq, n_embd] -> [n_seq, n_embd]
+def mha(x, c_attn, c_proj, n_head, kv_cache=None): # [n_seq, n_embd] -> [n_seq, n_embd]
     # qkv projections
     x = linear(x, **c_attn) # [n_seq, n_embd] @ [n_embd, 3*n_embd] -> [n_seq, 3*n_embd]
 
     # split into qkv
     qkv = np.split(x, 3, axis=-1) # [n_seq, 3*n_embd] -> 3 of [n_seq, n_embd]
 
+    # if kv_cache is passed, then append to them
+    if kv_cache is not None:
+        new_q, new_k, new_v = qkv # new_q, new_k, new_v = [1, n_embd]
+        old_k, old_v = kv_cache
+        k = np.vstack([old_k, new_k]) # k = [n_seq, n_embd], where n_seq = prev_n_seq + 1
+        v = np.vstack([old_v, new_v]) # v = [n_seq, n_embd], where n_seq = prev_n_seq + 1
+        qkv = [new_q, k, v]
+
+    current_cache = [qkv[1], qkv[2]]
+
     # split into heads
     qkv_heads = list(map(lambda x: np.split(x, n_head, axis=-1), qkv))  # [3, n_seq, n_embd] -> [3, n_head, n_seq, n_embd/n_head]
 
     # causal mask to hide future inputs from being attended to
-    causal_mask = (1 - np.tri(x.shape[0], dtype=x.dtype)) * -1e10  # [n_seq, n_seq]
+    if kv_cache:
+        causal_mask = np.zeros((1, k.shape[0]))
+    else:
+        causal_mask = (1 - np.tri(x.shape[0])) * -1e10  # [n_seq, n_seq]
 
     # perform attention over each head
     out_heads = [attention(q, k, v, causal_mask) for q, k, v in zip(*qkv_heads)]  # [3, n_head, n_seq, n_embd/n_head] -> [n_head, n_seq, n_embd/n_head]
@@ -99,19 +125,18 @@ def mha(x, c_attn, c_proj, n_head): # [n_seq, n_embd] -> [n_seq, n_embd]
     # out projection
     x = linear(x, **c_proj) # [n_seq, n_embd] @ [n_embd, n_embd] = [n_seq, n_embd]
 
-    return x
-
-
-
+    return x, current_cache
 
 
 def generate(inputs, params, n_head, n_tokens_to_generate):
+    kv_cache = None
+
     from tqdm import tqdm
 
     import time
     start = time.time()
     for _ in tqdm(range(n_tokens_to_generate), "generating"):  # auto-regressive decode loop
-        logits = gpt2(inputs, **params, n_head=n_head)  # model forward pass
+        logits, kv_cache = gpt2(inputs, **params, n_head=n_head, kv_cache=kv_cache)  # model forward pass
         next_id = np.argmax(logits[-1])  # greedy sampling
         inputs.append(int(next_id))  # append prediction to input
     end = time.time()
